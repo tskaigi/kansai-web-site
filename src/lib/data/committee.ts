049c5acf88508b34a31e7811038ec32aab962abd
So Nishimura <s.nishimura@gemcook.com>
2024-10-04 01:25:51 +0900

    refactor: x url

--- src/lib/data/committee.ts
+++ src/lib/data/committee.ts
@@ -14,12 +14,12 @@ export const selectionCommittee: Committee[] = [
     title: "うひょ",
     imageUrl:
       "https://pbs.twimg.com/profile_images/1341186424423378944/gYTKIbFl_400x400.jpg",
-    links: [{ icon: FaXTwitter, href: "https://twitter.com/uhyo_" }],
+    links: [{ icon: FaXTwitter, href: "https://x.com/uhyo_" }],
   },
   {
     title: "shisama",
     imageUrl:
       "https://pbs.twimg.com/profile_images/1591431637950418945/U97Oiuyq_400x400.jpg",
-    links: [{ icon: FaXTwitter, href: "https://twitter.com/shisama_" }],
+    links: [{ icon: FaXTwitter, href: "https://x.com/shisama_" }],
   },
 ];
