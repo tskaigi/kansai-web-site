1459f819dc361e50a6afaae4293c840f0bbe0d6a
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2024-10-23 01:43:06 +0900

    feat: add jobboard images

--- src/lib/data/jobBord.ts
+++ src/lib/data/jobBord.ts
@@ -1,37 +1,22 @@
 export const jobBoard = [
   {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
+    alt: "株式会社Helpfeel",
+    link: "https://recruit-engineer.helpfeel.com/",
+    imageLink: "JobBoard/G03_株式会社Helpfeel.png",
   },
   {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
+    alt: "株式会社カケハシ",
+    link: "https://hrmos.co/pages/kakehashi",
+    imageLink: "JobBoard/G04_株式会社カケハシ.jpg",
   },
   {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
+    alt: "カミナシ",
+    link: "https://careers.kaminashi.jp/",
+    imageLink: "JobBoard/G07_カミナシ.jpg",
   },
   {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
-  },
-  {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
-  },
-  {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
-  },
-  {
-    alt: "helpfeel",
-    link: "https://helpfeel.com/",
-    imageLink: "JobBoard/helpfeel.png",
+    alt: "LINEヤフー株式会社",
+    link: "https://www.lycorp.co.jp/ja/recruit/career/job-categories/#j-0-0",
+    imageLink: "JobBoard/S01_LINEヤフー株式会社.png",
   },
 ] as const;
