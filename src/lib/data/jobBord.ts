1732b62fa13918176bfebbef2d1fd8960b0c154d
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2024-10-24 14:20:38 +0900

    fix: image path

--- src/lib/data/jobBord.ts
+++ src/lib/data/jobBord.ts
@@ -2,31 +2,31 @@ export const jobBoard = [
   {
     alt: "株式会社LabBase",
     link: "https://labbase.co.jp/engineer/",
-    imageLink: "JobBoard/B01_株式会社LabBase.png",
+    imageLink: "/JobBoard/B01_株式会社LabBase.png",
   },
   {
     alt: "株式会社Helpfeel",
     link: "https://recruit-engineer.helpfeel.com/",
-    imageLink: "JobBoard/G03_株式会社Helpfeel.png",
+    imageLink: "/JobBoard/G03_株式会社Helpfeel.png",
   },
   {
     alt: "株式会社カケハシ",
     link: "https://hrmos.co/pages/kakehashi",
-    imageLink: "JobBoard/G04_株式会社カケハシ.jpg",
+    imageLink: "/JobBoard/G04_株式会社カケハシ.jpg",
   },
   {
     alt: "カミナシ",
     link: "https://careers.kaminashi.jp/",
-    imageLink: "JobBoard/G07_カミナシ.jpg",
+    imageLink: "/JobBoard/G07_カミナシ.jpg",
   },
   {
     alt: "newmo株式会社",
     link: "https://careers.newmo.me/",
-    imageLink: "JobBoard/G08_newmo株式会社.png",
+    imageLink: "/JobBoard/G08_newmo株式会社.png",
   },
   {
     alt: "LINEヤフー株式会社",
     link: "https://www.lycorp.co.jp/ja/recruit/career/job-categories/#j-0-0",
-    imageLink: "JobBoard/S01_LINEヤフー株式会社.png",
+    imageLink: "/JobBoard/S01_LINEヤフー株式会社.png",
   },
 ] as const;
